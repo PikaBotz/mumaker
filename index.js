38474ca8376c5e70eb5e4eb162d083e00259c222
Pika-Kun 3 <121213527+PikaBotz@users.noreply.github.com>
2025-01-05 10:28:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-exports.textpro = require("./src/maker")
-exports.photooxy = require("./src/maker")
-exports.ephoto = require("./src/maker")
-exports.tiktok = require("./src/downloader").tiktok
-exports.instagram = require("./src/downloader").instagram
-exports.facebook = require("./src/downloader").facebook
+exports.textpro = require("./src/maker");
+exports.photooxy = require("./src/maker");
+exports.ephoto = require("./src/maker");
